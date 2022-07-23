5f6ca532ff42177c576e7c5b273cc3e8cf0e633e
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-23 19:28:13 +0800

    Handling Errors with Promises - getCatch()

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -8,7 +8,24 @@ export function get() {
 }
 
 export function getCatch() {
+    axios.get("http://localhost:3000/orders/123") // order 123 is a non-existent order
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    }).catch(err => setText(err));   
 }
+
+/* Uncomment this to see 2 errors: 404 for getting order 123 and rejection for promise
+export function getCatch() {
+    axios.get("http://localhost:3000/orders/123") // order 123 is a non-existent order
+    .then((result) => {
+        if (result.status === 200) {
+            setText(JSON.stringify(result.data));
+        } else {
+            setText("Error");
+        }
+    });  
+}
+*/
 
 export function chain() {
 }
