54a88e7cd2f2fe91648284174f2fb4526c53b6b5
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-27 21:05:38 +0800

    Creating Promises

--- src/creating.mjs
+++ src/creating.mjs
@@ -1,6 +1,20 @@
 import setText, { appendText } from "./results.mjs";
 
 export function timeout(){
+    /*
+    The Promise constructor takes in 1 function as argument, called executor. 
+    The executor function takes in a param called resolve, which can be called to 
+    change the state of the promise to resolved state.  
+    */
+    const waiter = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("timeout liao!");
+        }, 1500);
+    });
+
+    waiter.then((text) => { // this is what happens after the promise is in fulfilled state
+        setText(text);
+    })
 }
 
 export function interval(){
