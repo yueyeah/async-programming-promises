8e7ddfa84bab0d5879800970afc7bc5a4ce15783
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-27 21:50:49 +0800

    understanding promise states

--- src/creating.mjs
+++ src/creating.mjs
@@ -7,20 +7,57 @@ export function timeout(){
     change the state of the promise to resolved state.  
     */
     const waiter = new Promise((resolve) => {
+        // setTimeout only fires once
         setTimeout(() => {
+            console.log("timeout has fired");
             resolve("timeout liao!");
         }, 1500);
     });
 
     waiter.then((text) => { // this is what happens after the promise is in fulfilled state
         setText(text);
-    })
+    });
 }
 
 export function interval(){
+    let counter = 0;
+    const waiter = new Promise((resolve) => {
+        // setInterval fires multiple times, unlike setTimeout
+        setInterval(() => {
+            console.log("interval is firing");
+            resolve(`timeout liao! ${++counter}`);
+        }, 1500);
+    });
+
+    waiter.then((text) => { // this is what happens after the promise is in fulfilled state
+        setText(text);
+    })
+    .finally(() => {
+        appendText(`--finally ${counter}--`);
+        /*
+        Using the counter here is no use, since the promise is already in a resolved state, and thus 
+        there will be no further change to the counter displayed in the browser. 
+        */
+    });
 }
 
 export function clearIntervalChain(){
+    let counter = 0;
+    let interval;
+    const waiter = new Promise((resolve) => {
+        // setInterval fires multiple times, unlike setTimeout
+        interval = setInterval(() => {
+            console.log("interval is firing");
+            resolve(`timeout liao! ${++counter}`);
+        }, 1500);
+    });
+
+    waiter.then((text) => { // this is what happens after the promise is in fulfilled state
+        setText(text);
+    })
+    .finally(() => {
+        clearInterval(interval);
+    });
 }
 
 export function xhr(){
