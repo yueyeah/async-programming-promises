07d1117dc1b9e69aed75e95c304d952d51eaac3e
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-27 22:48:25 +0800

    Rejecting a Promise

--- src/creating.mjs
+++ src/creating.mjs
@@ -61,6 +61,24 @@ export function clearIntervalChain(){
 }
 
 export function xhr(){
+    let request = new Promise((resolve, reject) => { // executor takes reject as second param
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:3000/users/7"); // this page doesn't exist, so shld return 404
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(xhr.responseText); // make it resolved status
+            } else {
+                reject(xhr.statusText); // make it rejected status, statusText is "NOT FOUND"
+            }
+        }; // this means that when xhr loads, promise will 
+        // enter the resolve state
+        xhr.onerror = () => reject("Request failed"); // on error means for network errors, not really 
+        // application error, which will also occur for onload()
+        xhr.send();
+    });
+
+    request.then((response) => setText(response))
+        .catch((rejectReason) => setText(rejectReason));
 }
 
 export function allPromises(){
