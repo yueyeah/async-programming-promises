e7a53f60b5d8358b464428ba048635027f2e9f81
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-29 14:58:07 +0800

    handling errors with async/await

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -5,7 +5,14 @@ export async function get(){
     setText(JSON.stringify(data));
 }
 
-export function getCatch(){
+export async function getCatch(){
+    try { // async-await can make use of normal try-catch for errors
+        const {data} = await axios.get("http://localhost:3000/orders/1123");
+        setText(JSON.stringify(data));
+    } catch (error) {
+        setText(error);
+    }
+    
 }
 
 export function chain(){
