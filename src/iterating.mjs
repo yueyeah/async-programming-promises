9213775d2177196282e14811b9e2a7449e28a4bf
Yue Jun Yi <yuejunyi@u.nus.edu>
2022-07-29 14:26:02 +0800

    Awaiting a call

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,6 +1,8 @@
 import setText , {appendText} from './results.mjs';
 
-export function get(){
+export async function get(){
+    const {data} = await axios.get("http://localhost:3000/orders/1");
+    setText(JSON.stringify(data));
 }
 
 export function getCatch(){
